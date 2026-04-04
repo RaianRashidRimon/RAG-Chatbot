db99999dbc1c133c22112b3404b95a1b9d16d1e5
Raian Rashid <rimonraian2@gmail.com>
2026-04-04 23:00:25 +0600

    clean up imports

--- rag-chatbot/app.py
+++ rag-chatbot/app.py
@@ -3,8 +3,6 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 from core.chain import build_qa_chain
 import tempfile
